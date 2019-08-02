c5b6a30e4e533a4a13f9ceeb731c2c5e856f66ba
ZhaoWenyue <jqzhaowenyue@163.com>
2019-08-02 13:21:58 +0800

    add

--- leet.go
+++ leet.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strings"
 )
 
 func relativeSortArray(arr1 []int, arr2 []int) []int {
@@ -36,10 +37,49 @@ func relativeSortArray(arr1 []int, arr2 []int) []int {
 	return result
 }
 
+//冒泡
+func Bubble(arr []int) []int{
+	for i := 0; i< len(arr);i++ {
+		for j := i+1; j< len(arr); j++  {
+			if arr[i] > arr[j]{
+				temp := arr[i]
+				arr[i] = arr[j]
+				arr[j] = temp
+			}
+		}
+	}
+	return arr
+}
+
+func SelectMax(arr []int) int{
+	maxValue := arr[0]
+	for i := 0; i<len(arr); i++  {
+		if arr[i] > maxValue{
+			maxValue = arr[i]
+		}
+	}
+	return maxValue
+}
+
+func defangIPaddr(address string) string {
+	return strings.ReplaceAll(address, ".", "[.]")
+}
+
+
+func sortedSquares(A []int) []int {
+	for i ,_ := range A {
+		A[i] *= A[i]
+	}
+	sort.Ints(A)
+	return A
+}
+
+
 func main() {
-	arr1 := []int{2,3,1,3,2,4,6,7,9,2,19}
-	arr2 := []int{2,1,4,3,9,6}
-	array := relativeSortArray(arr1, arr2)
-	fmt.Print(array)
+	//arr1 := []int{2,3,1,3,2,4,6,7,9,2,19}
+	//arr2 := []int{2,1,4,3,9,6}
+	address := "1.1.1.1"
+	//array := relativeSortArray(arr1, arr2)
+	fmt.Print(defangIPaddr(address))
 }
 
